2537b38d662e3e5019abe895ce678df2dca58e6c
Lucas <lucas.av7@yahoo.com>
2020-05-08 12:08:03 -0400

    Adicionado menudropdown responsivo

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+const botaoOpen = document.getElementById('menuOpen');
+const botaoClose = document.getElementById('menuClose');
+const menu = document.querySelector('.menuPrincipal')
+
+botaoOpen.addEventListener('click', abrirMenu);
+botaoClose.addEventListener('click', fecharMenu);
+
+function abrirMenu() {
+    menu.style.top = '50px';
+    botaoOpen.style.display = 'none';
+    botaoClose.style.display = 'inline-block';
+}
+
+function fecharMenu() {
+    menu.style.top = '0px';
+    botaoOpen.style.display = 'inline-block';
+    botaoClose.style.display = 'none';
+}
